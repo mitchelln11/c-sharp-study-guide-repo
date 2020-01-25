545c3f696e2d00ceb2acd19401d66a7191afb50d
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-01-25 15:20:38 -0600

    added proxy pattern

---
+++ cSharpStudyGuide/cSharpStudyGuide/ProxyPatterrn/ComponentPriceProxy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace cSharpStudyGuide.ProxyPatterrn
+{
+    public class ComponentPriceProxy : IComponentPrice
+    {
+        private decimal MakeRequest(string component)
+        {
+            using (var client = new TcpClient())
+            {
+                client.Connect("127.0.0.1", 8080);
+
+                var stream = client.GetStream();
+                var data = Encoding.Default.GetBytes(component);
+
+                stream.Write(data, 0, data.Length);
+
+                var rawResponse = new byte[15];
+                stream.Read(rawResponse, 0, rawResponse.Length);
+
+                var response = Encoding.Default.GetString(rawResponse);
+
+                return Convert.ToDecimal(response);
+            }
+        }
+
+        public decimal CpuPrice
+        {
+            get { return MakeRequest("cpu"); }
+        }
+
+        public decimal RamPrice
+        {
+            get { return MakeRequest("ram"); }
+        }
+
+        public decimal SsdPrice
+        {
+            get { return MakeRequest("ssd"); }
+        }
+
+        public void RunProxy()
+        {
+            var prices = new StoredComponentPrices();
+
+            Console.WriteLine("CPU Price: {0}", prices.CpuPrice);
+            Thread.Sleep(500);
+            Console.WriteLine("CPU Price: {0}", prices.RamPrice);
+            Thread.Sleep(500);
+            Console.WriteLine("CPU Price: {0}", prices.SsdPrice);
+            Thread.Sleep(500);
+        }
+    }
+}
---
+++ cSharpStudyGuide/cSharpStudyGuide/ProxyPatterrn/StoredComponentPrices.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpStudyGuide.ProxyPatterrn
+{
+    public class StoredComponentPrices : IComponentPrice
+    {
+
+        public decimal CpuPrice
+        {
+            get { return 250m; }
+        }
+
+        public decimal RamPrice
+        {
+            get { return 32m; }
+        }
+
+        public decimal SsdPrice
+        {
+            get { return 225m; }
+        }
+    }
+}
