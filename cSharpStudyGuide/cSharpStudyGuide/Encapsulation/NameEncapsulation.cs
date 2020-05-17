eba87e011f55c5a2837479920dc501fb884cc314
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-05-17 18:48:37 -0500

    added different examples of encapsulation

---
+++ cSharpStudyGuide/cSharpStudyGuide/Encapsulation/NameEncapsulation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharpStudyGuide.Encapsulation
+{
+    class NameEncapsulation
+    {
+        // I've been trying to find examples of how to add multiple default values, like, "Tim", "Tom", and "Tony, but I can't seem to find any
+        // I think they're better suited for a single default value, like a beginning bank account amount.
+
+
+        // Updated, short-hand way to set up encapsulation --  Works
+        public string Name { get; set; }
+
+        //// Old way of setting up private info and using getter/setter Properties -- This works below
+        //private string name;
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+
+
+        //-----------------------------------------------------------------------
+
+        ////Assigned value from the start
+        //private string name = "Teddy";
+
+        //public string Name
+        //{
+        //    get { return name; }
+        //    set { name = value; }
+        //}
+    }
+}
--- cSharpStudyGuide/cSharpStudyGuide/Program.cs
+++ cSharpStudyGuide/cSharpStudyGuide/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using cSharpStudyGuide.Tests;
+using cSharpStudyGuide.Encapsulation;
 
 
 namespace cSharpStudyGuide
@@ -12,9 +13,40 @@ namespace cSharpStudyGuide
     {
         static void Main(string[] args)
         {
+            ////////////////// DIFFERENT FOLDER EXAMPLE BELOW /////////////////////
             // Must add using statement with namespace AND folder name
-            Vendor v = new Vendor();
-            v.buySoda();
+            //Vendor v = new Vendor();
+            //v.buySoda();
+
+
+
+            ////////////////// NameEncapsulation EXAMPLES BELOW /////////////////////
+
+            NameEncapsulation person = new NameEncapsulation();
+            person.Name = "Timmy";
+            Console.WriteLine(person.Name);
+            Console.ReadLine();
+
+            //-----------------------------------------------------------------------
+
+            //// You can add multiples this way
+            //NameEncapsulation person = new NameEncapsulation();
+            //person.Name = "Timmy";
+
+            //NameEncapsulation person2 = new NameEncapsulation();
+            //person2.Name = "Tony";
+
+            //Console.WriteLine(person.Name); // Must call the variable, or you will just get cSharpStudyGuide, Encapsulation, NameEncapsulation
+            //Console.WriteLine(person2.Name);
+            //Console.ReadLine();
+
+            //-----------------------------------------------------------------------
+
+            // If something already exists, or was declared in the private variable, you can access it this way.
+            //NameEncapsulation person = new NameEncapsulation();
+
+            //Console.WriteLine(person.Name);
+            //Console.ReadLine();
         }
     }
 }
